de1f8bcaf523b25d736b8c89a1b7b022f65a8b39
Akhaled19 <akhaled@clarku.edu>
2020-02-20 00:13:29 -0500

    created config.js

---
+++ src/config.js
@@ -0,0 +1,14 @@
+//NOTE: this file must be listed in the gitignore file 
+
+//import API key 
+//export API key 
+//import this file to src/App.js
+    //save it to a variable like you would any other module 
+    //use the variable where applicable 
+        //Goal: your app's users will only need to enter in an API key once
+//The file should look something like this 
+
+/* 
+const apiKey = 'YOUR API KEY';
+export default apiKey
+*/
