68d0ef2c31a53cc5ebff2f38349736a0245c3a3d
Akhaled19 <akhaled@clarku.edu>
2020-03-10 14:15:47 -0400

    image opens on a new window

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -6,7 +6,7 @@ import React from 'react';
 const Photo = (props) => {
         return(
             <li>
-                <img src={props.url} alt='' />
+                <img src={props.url} alt='' onClick={() => window.open(props.url)}/>
             </li>
         );
     
