3a8c0c080dcd8f20d5e1cfe4e3c1bc5e744e2bf0
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-15 15:34:03 -0400

    add logic to resizing the background image & nav buttons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ const Nav = (props) => {
     
     return(
             <nav className='main-nav'>
-                <ul className='.list-inline'>
+                <ul className='list-inline'>
                     <li><NavLink to='/sea' onClick={ () => props.fetchData('sea')}>Sea</NavLink></li>
                     <li><NavLink to='/clouds' onClick={ () => props.fetchData('pink cloud')}>Clouds</NavLink></li>
                     <li><NavLink to='/nature' onClick={ () => props.fetchData('nature')}>Nature</NavLink></li>
